5856daf650eccdc39f64ba92514130c98168e2d8
Vasyl Strutynskyy <strutv@Vasyl-Strutynskyys-MacBook-Pro.local>
2012-10-02 17:09:10 +0300

    canvas and image scale

--- tests/test1/script.js
+++ tests/test1/script.js
@@ -51,7 +51,7 @@ window.addEventListener('load', function () {
       var rowIndex;
       for (rowIndex = 0; rowIndex < cols; rowIndex ++) {
         bmp = new createjs.Bitmap(img);
-        bmp.scaleX = bmp.scaleY = 0.1;
+        bmp.scaleX = bmp.scaleY = 0.3;
         bmp.y = cellHeight * (colIndex + rowIndex);
         bmp.x = cellWidth  * rowIndex;
         bmps.push(bmp);
